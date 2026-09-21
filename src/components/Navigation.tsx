ff319bd817251ee0ab33b5f3d85b15ffe70eaa4b
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-21 11:26:05 +0900

    feat: Implement Navigation Components

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router";
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/settings">Settings</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
